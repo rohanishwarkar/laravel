9488d86e941394d44d1d3324519749ce20f4145f
Rohan Ishwarkar <rohan9421837751@gmail.com>
2020-05-03 18:49:01 +0530

    Email Verification

--- resources/views/verifyEmail.php
+++ resources/views/verifyEmail.php
@@ -4,10 +4,10 @@
 
 <body>
 	
-<p>Dear {{ $user->name }}</p>
+<p>Dear <? $user->name ?></p>
 <p>Your account has been created, please activate your account by clicking this link</p>
-<p><a href="{{ route('verify',$user->email_verification_token) }}">
-	{{ route('verify',$user->email_verification_token) }}
+<p><a href="<?route('verify',$user->email_verification_token)?>">
+	<?route('verify',$user->email_verification_token)?>
 </a></p>
 
 <p>Thanks</p>
