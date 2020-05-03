32d1e24b86ba9627346d1094e82d2aca3195d03f
Rohan Ishwarkar <rohan9421837751@gmail.com>
2020-05-03 14:17:41 +0530

    Email Verification

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Auth::routes(['verify' => true]);
 // Route::resource('/products','ProductController');
 Route::post('/register','authcon@register');
 Route::post('/login','authcon@login');
